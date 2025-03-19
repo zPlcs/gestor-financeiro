37de2957f8079f814b6193189f54c038f72abe33
Patrik Luiz <111705938+zPlcs@users.noreply.github.com>
2025-03-19 00:36:28 -0300

    Testing

---
+++ src/style/style.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native'
+
+export default style = StyleSheet.create({
+    main:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+})
