835551d1c17fc27a91e04130c8d708dcf95c00aa
Patrik Luiz <111705938+zPlcs@users.noreply.github.com>
2025-03-20 08:39:12 -0300

    Criar NavButton.js

---
+++ src/components/NavButton.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { TouchableOpacity, Text, View, StyleSheet } from 'react-native'
+
+export default function NavButtom(props){
+    return(
+        <View style={Style.container}>
+        <TouchableOpacity onPress={props.func}>
+            <Text>{props.title}</Text>
+        </TouchableOpacity>
+        </View>
+    );
+}
+
+const Style = StyleSheet.create((
+    container:{
+        flex: 1,
+        justifyContent:'center',
+        alignaitems:'center',
+    },
+    
+    button:{
+        borderWidth: 1,
+        borderColor: 'black'
+    },
+))
