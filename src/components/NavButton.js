23fe2902e581f7cefd0105397327759b4090db6c
Patrik Luiz <patrikluiz2004@hotmail.com>
2025-03-25 10:06:28 -0300

    Update NavButton.js

--- src/components/NavButton.js
+++ src/components/NavButton.js
@@ -1,25 +1,29 @@
 import React from 'react'
-import { TouchableOpacity, Text, View, StyleSheet } from 'react-native'
+import { TouchableOpacity, Text, StyleSheet, Dimensions, View } from 'react-native'
 
-export default function NavButtom(props){
-    return(
-        <View style={Style.container}>
-        <TouchableOpacity onPress={props.func}>
-            <Text>{props.title}</Text>
-        </TouchableOpacity>
-        </View>
-    );
+export default function NavButton(){
+  return(
+    <View style={Style.container}>
+      <TouchableOpacity style={Style.button}>
+        <Text>+</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
 
 const Style = StyleSheet.create({
-    container:{
-        flex: 1,
-        justifyContent:'center',
-        alignaitems:'center',
-    },
-    
-    button:{
-        borderWidth: 1,
-        borderColor: 'black'
-    },
+  button:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 2,
+    borderColor: 'black',
+    width:Dimensions.get('window').width / 6
+  },
+
+  container:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 })
