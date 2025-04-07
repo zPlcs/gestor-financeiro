896f8b9e64fb6b42c6966b62f4cece89470d8aef
Patrik Luiz <patrikluiz2004@hotmail.com>
2025-04-07 16:46:48 -0300

    MainContext fixed

--- src/context/MainContext.js
+++ src/context/MainContext.js
@@ -1,17 +1,39 @@
 import React, { createContext, useState, useEffect } from 'react';
 import {
-    initDB,
-    addDivida,
-    getDivida,
-    deleteDivida,
-    updateDivida,
-    getCategorys,
-    addCategory,
+    //DATABASE
     deleteDatabase,
+    initDB,
+
+    //LISTAS
     createList,
-    getLists,
+    getList,
+    updateList,
     deleteList,
-    updateList
+
+    //DIVIDAS
+    createDebt,
+    getDebt,
+    updateDebt,
+    deleteDebt,
+
+    //ITENS
+    createItem,
+    getItem,
+    updateItem,
+    deleteItem,
+
+    //CATEGORIAS - DIVIDAS
+    createCategoryDebt,
+    getCategoryDebt,
+    updateCategoryDebt,
+    deleteCategoryDebt,
+
+    //CATEGORIAS - ITENS
+    createCategoryItem,
+    getCategoryItem,
+    updateCategoryItem,
+    deleteCategoryItem,
+
 } from '../database/database'
 
 
@@ -21,147 +43,289 @@ export function MainProvider({ children }) {
     useEffect(() => {
         const loadData = async () => {
             await initDB();
-            const listsFromDB = await getLists();
-            const dividasFromDB = await getDivida();
-            const categorysFromDB = await getCategorys();
-            setCategorys(categorysFromDB);
-            setDividas(dividasFromDB);
-            setLists(listsFromDB);
+
+            const listsFromDB = await getList();
+            const debtsFromDB = await getDebt();
+            const itemsFromDB = await getItem();
+            const categorysDebtFromDB = await getCategoryDebt();
+            const categorysItemFromDB = await getCategoryItem();
+
+            setList(listsFromDB);
+            setDebt(debtsFromDB);
+            setItem(itemsFromDB);
+            setCategorysDebtFromDB(categorysDebtFromDB);
+            setCategorysItemFromDB(categorysItemFromDB);
         };
         loadData();
     }, []);
 
-// CRUD (CREATE)
-    const criarList = async (newList) => {
-        try{
-            await createList(newList);
-            const updatedLists = await getLists();
-            setLists(updatedLists);
-        } catch(error){
-            console.error('Falha ao criar lista (Func. criarList() => MainContext.js)', error)
-        }
-    }
-
-// CRUD (READ)
-
-// CRUD (UPDATE)
-
-const editarDivida = async (id, novosDados) => {
-    try {
-        await updateDivida(id, novosDados);
-        const updatedDividas = await getDivida();
-        setDividas(updatedDividas);
-    } catch (error) {
-        console.error('Erro ao editar')
-    }
-}
-
-const atualizarList = async (id, novosDados) => {
-    try{
-        await updateList(id, novosDados);
-        const updatedLists = await getLists();
-        setLists(updatedLists);
-    } catch(error){
-        console.error('Erro ao atualizar lista (Func. atualizarList() => MainContext.js', error)
-    }
-}
-
-// (CRUD) (DELETE)
-
-const deletarDivida = async (id) => {
-    try {
-        await deleteDivida(id);
-        const updatedDividas = await getDivida();
-        setDividas(updatedDividas);
-    } catch (error) {
-        console.error('Erro ao tentar excluir', error)
-    }
-}
-
-const deletarList = async (id) => {
-    try{
-        await deleteList(id);
-        const updatedLists = await getLists();
-        setLists(updatedLists);
-    } catch(error){
-        console.error('Erro ao apagar a lista', error)
-    }
-}
-
-    const criarDivida = async (novaDivida) => {
-        try {
-            await addDivida(novaDivida);
-            const updatedDividas = await getDivida();
-            setDividas(updatedDividas);
-        } catch (error) {
-            console.error('Falha ao criar divida (Func. criarDivida() => MainContext.js):', error);
-        }
-    };
-
-    const deletarBanco = async () => {
-        try {
-            await deleteDatabase()
-        } catch (error) {
-            console.error('banco nãod deletado', error)
-        }
-    }
-
-
-
-
-
-    const criarCategory = async (newCategory) => {
-        try {
-            await addCategory(newCategory);
-            const updatedCategorys = await getCategorys();
-            setCategorys(updatedCategorys);
-        } catch (error) {
-            console.error('Falha ao criar categoria (Func. criarCategory() => MainContext.js):', error);
-        }
-    };
-
-    const getCategoryName = (categoryId) => {
-        const category = categorys.find(cat => cat.id === categoryId);
-        return category ? category.name : "Sem categoria";
-    };
-
-
+    // ESTADOS
     const [name, setName] = useState('');
     const [value, setValue] = useState(0);
     const [date, setDate] = useState(new Date());
-    const [categoryName, setCategoryName] = useState('');
-    const [categorys, setCategorys] = useState([]);
-    const [dividas, setDividas] = useState([]);
 
-    const [nameList, setNameList] = useState('');
-    const [lists, setLists] = useState([]);
+    // ARRAYS
+    const [list, setList] = useState([]);
+    const [divida, setDivida] = useState([]);
+    const [item, setItem] = useState([]);
+    const [categoryDebt, setCategoryDebt] = useState([]);
+    const [categoryItem, setCategoryItem] = useState([]);
 
-    function ClearFormDivida() {
+    // CRUD LISTAS
+
+    const criarLista = async (novaLista) => {
+        try {
+            await createList(novaLista);
+            const listaAtualizada = await getList();
+            setList(listaAtualizada);
+        } catch (error) {
+            console.error('Erro ao criar lista', error)
+        }
+    }
+
+    const atualizarLista = async (id, novaLista) => {
+        try {
+            await updateList(id, novaLista);
+            const listaAtualizada = await getList();
+            setList(listaAtualizada);
+        } catch (error) {
+            console.error('Erro ao atualizar lista', error)
+        }
+    }
+
+    const deletarLista = async (id) => {
+        try {
+            await deleteList(id);
+            const listaAtualizada = await getList();
+            setList(listaAtualizada);
+        } catch (error) {
+            console.error('Erro ao deletar lista', error)
+        }
+    }
+
+    // CRUD LISTAS
+
+    // CRUD DIVIDAS
+
+    const criarDivida = async (listaId, novaDivida) => {
+        try {
+            await createDivida(listaId, novaDivida);
+            const dividaAtualizada = getDebt();
+            setDivida(dividaAtualizada);
+        } catch (error) {
+            console.error('Erro ao criar divida', error)
+        }
+    }
+
+    const atualizarDivida = async (id, novaDivida) => {
+        try {
+            await updateDebt(id, novaDivida);
+            const dividaAtualizada = getDebt();
+            setDivida(dividaAtualizada);
+        } catch (error) {
+            console.error('Erro ao atualizar a divida', error)
+        }
+    }
+
+    const deletarDivida = async (id) => {
+        try {
+            await deleteDebt(id);
+            const dividaAtualizada = getDebt();
+            setDivida(dividaAtualizada)
+        } catch (error) {
+            console.error('Erro ao deletar a divida', error)
+        }
+    }
+
+    // CRUD DIVIDAS
+
+    // CRUD ITENS
+
+    const criarItem = async (listaId, novoItem) => {
+        try {
+            await createItem(listaId, novoItem);
+            const itemAtualizado = getItem();
+            setItem(itemAtualizado)
+        } catch (error) {
+            console.error('Erro ao criar item', error)
+        }
+    }
+
+    const atualizarItem = async (id, novoItem) => {
+        try {
+            await updateItem(id, novoItem);
+            const itemAtualizado = getItem();
+            setItem(itemAtualizado);
+        } catch (error) {
+            console.error('Erro ao atualizar item', error)
+        }
+    }
+
+    const deletarItem = async (id) => {
+        try {
+            await deleteItem(id);
+            const itemAtualizado = getItem();
+            setItem(itemAtualizado);
+        } catch (error) {
+            console.error('Erro ao deletar item', error)
+        }
+    }
+
+    // CRUD ITENS
+
+    // CRUD CATEGORIAS (DIVIDAS)
+
+    const criarCategoriaDivida = async (dividaId, novaCategoriaDivida) => {
+        try {
+            await createCategoryDebt(dividaId, novaCategoriaDivida);
+            const categoriaDividaAtualizada = getCategoryDebt();
+            setCategoryDebt(categoriaDividaAtualizada)
+        } catch (error) {
+            console.error('Erro ao criar categoria de divida', error)
+        }
+    }
+
+    const atualizarCategoriaDivida = async (id, novaCategoriaDivida) => {
+        try {
+            await updateCategoryDebt(id, novaCategoriaDivida);
+            const categoriaDividaAtualizada = getCategoryDebt();
+            setCategoryDebt(categoriaDividaAtualizada)
+        } catch (error) {
+            console.error('Erro ao atualizar categoria de divida', error)
+        }
+    }
+
+    const deletarCategoriaDivida = async (id) => {
+        try {
+            await deleteCategoryDebt(id);
+            const categoriaDividaAtualizada = getCategoryDebt();
+            setCategoryDebt(categoriaDividaAtualizada)
+        } catch (error) {
+            console.error('Erro ao deletar categoria de divida', error)
+        }
+    }
+
+    // CRUD CATEGORIAS (DIVIDAS)
+
+    // CRUD CATEGORIAS (ITENS)
+
+    const criarCategoriaItem = async (dividaId, novaCategoriaItem) => {
+        try {
+            await createCategoryItem(dividaId, novaCategoriaItem);
+            const categoriaItemAtualizada = getCategoryItem();
+            setCategoryItem(categoriaItemAtualizada)
+        } catch (error) {
+            console.error('Erro ao criar categoria do item', error)
+        }
+    }
+
+    const atualizarCategoriaItem = async (id, novaCategoriaItem) => {
+        try {
+            await updateCategoryItem(id, novaCategoriaItem);
+            const categoriaItemAtualizada = getCategoryItem();
+            setCategoryItem(categoriaItemAtualizada);
+        } catch (error) {
+            console.error('Erro ao atualizar categoria do item', error)
+        }
+    }
+
+    const deletarCategoriaItem = async (id) => {
+        try {
+            await deleteCategoryItem(id);
+            const categoriaItemAtualizada = getCategoryItem();
+            setCategoryItem(categoriaItemAtualizada);
+        } catch (error) {
+            console.error('Erro ao deletar categoria do item', error)
+        }
+    }
+
+    // CRUD DB
+
+    const deletarBD = async () => {
+        try {
+            await deleteDatabase()
+        } catch (error) {
+            console.error('banco não deletado', error)
+        }
+    }
+
+    // CRUD DB
+
+    // FUNÇÕES
+
+    function LimparFormLista() {
+        setName('');
+    }
+
+    function LimparFormDivida() {
         setName('');
         setValue(0);
         setDate(new Date());
     }
 
-    function ClearFormCategoria() {
-        setCategoryName('');
+    function LimparFormItem() {
+        setName('');
+        setValue(0);
     }
+
+    function LimparFormCategoriaDivida() {
+        setCategoryDivida('');
+    }
+
+    function LimparFormCategoriaItem() {
+        setCategoryItem('');
+    }
+
+    // FUNÇÕES
 
     return (
         <MainContext.Provider value={{
+            // CRUD LISTAS
+            criarLista,
+            atualizarLista,
+            deletarLista,
+
+            // CRUD DIVIDAS
+            criarDivida,
+            atualizarDivida,
+            deletarDivida,
+
+            // CRUD ITENS
+            criarItem,
+            atualizarItem,
+            deletarItem,
+
+            //CRUD CATEGORIAS > DIVIDAS
+            criarCategoriaDivida,
+            atualizarCategoriaDivida,
+            deletarCategoriaDivida,
+
+            // CRUD CATEGORIAS > ITENS
+            criarCategoriaItem,
+            atualizarCategoriaItem,
+            deletarCategoriaItem,
+
+            // CRUD DATABASE
+            deletarBD,
+
+            // FUNÇÕES
+            LimparFormLista,
+            LimparFormDivida,
+            LimparFormItem,
+            LimparFormCategoriaDivida,
+            LimparFormCategoriaItem,
+
+            // ESTADOS
             name, setName,
             value, setValue,
             date, setDate,
-            categoryName, setCategoryName,
-            criarDivida, deletarDivida, editarDivida,
-            ClearFormDivida, ClearFormCategoria,
-            dividas, setDividas,
-            criarCategory, categorys,
-            getCategoryName,
-            deletarBanco,
-            criarList,
-            nameList, setNameList,
-            lists, deletarList,
-            atualizarList
+
+            // ARRAYS
+            list, setList,
+            divida, setDivida,
+            item, setItem,
+            categoryDebt, setCategoryDebt,
+            categoryItem, setCategoryItem
         }}>
             {children}
         </MainContext.Provider>
