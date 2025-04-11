2aa82a60f26c842af77d99aa78f1533c63d2cd8d
Patrik Luiz <patrikluiz2004@hotmail.com>
2025-04-11 17:33:08 -0300

    TESTE

--- src/screens/screensListas/ConfigurarListaCompras.js
+++ src/screens/screensListas/ConfigurarListaCompras.js
@@ -1,78 +1,12 @@
-import React, { useContext, useEffect } from 'react'
-import { SafeAreaView, View, Text, TextInput, Button, FlatList, Alert } from 'react-native'
+import React, {useContext} from 'react'
+import { SafeAreaView, Text, button } from 'react-native'
+import { MainContext } from '../../context/MainContext';
 import { useNavigation, useRoute } from '@react-navigation/native';
-import { MainContext } from '../../context/MainContext'
-import RenderItens from '../screensItens/RenderItens'
 
-export default function ConfigurarListaCompras() {
-
-    const navigation = useNavigation();
-
-    const { items, categoryItem } = useContext(MainContext)
-
-    const route = useRoute();
-    const {
-        listId,
-        listName,
-        listTemplate
-    } = route.params;
-
-    const itemId = categoryItem.find(cat => cat.id)
-
-    const handleCriarItem = () => {
-        if (itemId) {
-            navCriarItens()
-        } else {
-            Alert.alert('Nenhuma categoria criada', `Para criar uma divida, primeiro crie uma categoria.`, [
-                {
-                    text: 'Ok',
-                    onPress: () => { },
-                    style: 'cancel',
-                }
-            ]);
-        }
-    }
-
-    const navCriarItens = () => {
-        navigation.navigate('CriarItem', {
-            listId: listId,
-            listName: listName
-        })
-    }
-
-    const handleCategoriasItens = () => {
-        navigation.navigate('CategoriasItens')
-    }
-
-    const key = (p) => {
-        return p.id
-    }
-
-    return (
+export default function ConfigurarListasCompras(){
+    return(
         <SafeAreaView>
-            <Text>Configurar Lista</Text>
-            <Text>Você está editando a lista {listName} - {listId} - {listTemplate}</Text>
-            <Button
-                title='Adicionar Item'
-                onPress={handleCriarItem}
-            />
-            <Button
-                title='Configurar Categorias de itens'
-                onPress={handleCategoriasItens}
-            />
-
-            {items ? (
-                <FlatList
-                    style={{ height: 400 }}
-                    data={items}
-                    keyExtractor={key}
-                    renderItem={({ item }) => <RenderItens item={item} listId={listId} />}
-                />
-            ) : (
-                <Text>
-                    Nenhuma dívida cadastrada nesta lista
-                </Text>
-            )}
+            <Text>Confirgurar Lista Compras</Text>
         </SafeAreaView>
     );
 }
